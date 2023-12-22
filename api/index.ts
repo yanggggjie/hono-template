01f9ad48fe6811a2e61485af516b3c3a8ac1cb77
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-12-22 10:24:46 +0800

    add .env

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { env } from 'hono/adapter'
 
 export const config = {
   runtime: 'edge',
@@ -8,6 +9,8 @@ export const config = {
 const app = new Hono().basePath('/api')
 
 app.get('/', async (c) => {
+  const { TEST } = env(c)
+  console.log('TEST', TEST)
   return c.json({ message: 'Hello Hono123!' })
 })
 
